66cd40dac195c8bd461b13e8245b3b589a1ce0ef
ValentinCequeira <Valentincequeira@gmail.com>
2022-05-24 16:27:40 -0300

    first commit

---
+++ FBTarjeta/FBTarjeta/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using FBTarjeta.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBTarjeta
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public virtual DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //En caso de que el contexto no este configurado, lo configuramos mediante la cadena de conexion
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-QERHAT3\\SQLEXPRESS;Initial Catalog=TarjetaCreditoDB; Trusted_Connection=true;");
+            }
+        }
+    }
+}
